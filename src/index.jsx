dd9719955a431e14f425e6336fd16504301216a4
Sagar Deep <oceakun@pop-os.localdomain>
2023-06-28 01:22:55 +0530

    refactor: provide context to the whole app

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AnnotationsArrayContextProvider } from "./contexts/AnnotationsArrayContext";
+import { LabelInputContextProvider } from "./contexts/LabelInputContext";
+import { FormatValuesContextProvider } from "./contexts/FormatValuesContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-  ,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AnnotationsArrayContextProvider>
+    <LabelInputContextProvider>
+      <FormatValuesContextProvider>
+        <App />
+      </FormatValuesContextProvider>
+    </LabelInputContextProvider>
+  </AnnotationsArrayContextProvider>
+);
